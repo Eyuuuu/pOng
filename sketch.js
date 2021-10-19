9ca15eef0b49692ae8515b34d278936e459a2ecb
Eyuuuu <92765490+Eyuuuu@users.noreply.github.com>
2021-10-18 19:57:35 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background("purple");
+  rect(70,200,20,100);
+  rect(270,mouseY,20,100);
+  rect(200,200,20,20);
+  
+}
